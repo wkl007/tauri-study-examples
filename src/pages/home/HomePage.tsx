8b9dd09e60e0200e9252908e4b58524af612b98b
wkl007 <499657357@qq.com>
2023-01-01 13:33:51 +0800

    feat: 添加 http 请求示例

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,5 +1,25 @@
+import { useNavigate } from 'react-router-dom';
+import { Button } from 'antd';
+
+const pages = [
+  {
+    name: '1. 接口请求',
+    url: '/http',
+  },
+];
+
 const HomePage = () => {
-  return <div>home</div>;
+  const navigate = useNavigate();
+
+  return (
+    <>
+      {pages.map((page, index) => (
+        <Button key={index} onClick={() => navigate(page.url)}>
+          {page.name}
+        </Button>
+      ))}
+    </>
+  );
 };
 
 export default HomePage;
--- src/pages/http/HttpPage.tsx
+++ src/pages/http/HttpPage.tsx
@@ -1,5 +1,76 @@
+import { Button } from 'antd';
+import { Body } from '@tauri-apps/api/http';
+import http from '@/utils/http';
+
 const HttpPage = () => {
-  return <div>http</div>;
+  const getMethod = async () => {
+    try {
+      const params = {
+        key: '4fed42ec86c04922bf872b6f48809ebb',
+        location: '101010100',
+      };
+      const res = await http('https://devapi.qweather.com/v7/weather/now', {
+        method: 'get',
+        params,
+      });
+      console.log(res);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const postMethod = async () => {
+    try {
+      const body = Body.json({
+        test: '1',
+      });
+      const res = await http('https://devapi.qweather.com/post', {
+        method: 'post',
+        body,
+      });
+      console.log(res);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const generateFileInfo = (
+    arrayBuffer: any,
+    mime: string,
+    fileName: string,
+  ) => {
+    return {
+      file: new Uint8Array(arrayBuffer),
+      mime,
+      fileName,
+    };
+  };
+
+  const uploadMethod = async ({ target: { files } }: any) => {
+    try {
+      const [file] = files;
+      const arrayBuffer = await file.arrayBuffer();
+      const body = Body.form({
+        file: generateFileInfo(arrayBuffer, file.type, file.name),
+        // todo 其他参数
+      });
+      const res = await http('https://devapi.qweather.com/upload', {
+        method: 'post',
+        body,
+      });
+      console.log(res);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  return (
+    <div>
+      <Button onClick={getMethod}>GET 请求</Button>
+      <Button onClick={postMethod}>POST 请求</Button>
+      <input type="file" onChange={uploadMethod} />
+    </div>
+  );
 };
 
 export default HttpPage;
