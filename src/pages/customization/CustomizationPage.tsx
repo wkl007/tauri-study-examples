db10a048a2e13a69ef867cee8997df74557b8084
wkl007 <499657357@qq.com>
2023-07-18 15:11:16 +0800

    feat: add chatdoc example

--- src/pages/customization/CustomizationPage.tsx
+++ src/pages/customization/CustomizationPage.tsx
@@ -3,10 +3,20 @@ import { WebviewWindow } from '@tauri-apps/api/window';
 import { appWindow } from '@tauri-apps/api/window';
 import '@/assets/styles/pages/customization-page.less';
 
+const userAgent = {
+  macos:
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15',
+  linux:
+    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+  windows:
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+};
+
 const CustomizationPage = () => {
   const createWindow = () => {
-    const window = new WebviewWindow('bilibili', {
-      url: 'https://www.bilibili.com/',
+    const isMacOS = import.meta.env.TAURI_PLATFORM === 'macos';
+    const window = new WebviewWindow('chatdoc', {
+      url: 'https://bohr.cheftin.cn:8081/chatdoc/',
       title: '窗口定制',
       fullscreen: false,
       width: 800,
@@ -16,6 +26,7 @@ const CustomizationPage = () => {
       skipTaskbar: false,
       visible: false,
       decorations: true,
+      userAgent: isMacOS ? userAgent.macos : userAgent.windows,
     });
     window.once('tauri://created', () => {
       window.show();
